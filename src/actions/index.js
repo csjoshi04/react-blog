3881dc07d31de195b0d4f917236296bf97808669
Chandra Shekhar Joshi <cj.github@gmail.com>
2019-03-25 22:55:42 +0530

    using lodash chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,13 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const getPostsAndUsers = () => async (dispatch,getState) =>{
     await dispatch(getPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id=>dispatch(getUser(id)));
+    /* const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id=>dispatch(getUser(id))); */
+    _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id=>dispatch(getUser(id)))
+    .value();
 }
 
 export const getPosts = ()=>{
